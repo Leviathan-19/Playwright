e138667dd9e846538aeb4d098f8596e5b177314b
Leviathan19 <msnarvaez19@hotmail.com>
2026-04-28 16:37:38 -0500

    actualización de index.ts con console.logs para validar flujo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import 'dotenv/config';
+import { launchBrowser } from './core/browser';
+import { hasSession } from './core/session';
+import { login } from './services/auth.service';
+import { saveSession } from './core/session';
 
 (async () => {
-  console.log('Iniciando scraper...');
-})();
+  const context = await launchBrowser();
+  const page = await context.newPage();
 
+  await page.goto('https://www.instagram.com/');
+
+  const isLoggedIn = await page.locator('svg[aria-label="Home"]').count();
+
+  if (!isLoggedIn) {
+    console.log('No hay sesión activa, iniciando login...');
+    await login(page);
+
+    await saveSession(context);
+  } else {
+    console.log('Sesión ya activa');
+  }
+
+  await page.waitForTimeout(10000);
+})();
